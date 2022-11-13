3e2ebcf9a6838edf6201e46e2060721f137652d3
Ulya <uleiven@mail.ru>
2022-11-13 22:12:21 +0300

    V

---
+++ Ex5/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(
+    username =="Маша"
+    )
+
+{
+    Console.WriteLine("Ура,это же Маша");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
