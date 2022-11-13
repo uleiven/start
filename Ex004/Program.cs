de7515decea98c4332592ea71c3cb704812feefd
Ulya <uleiven@mail.ru>
2022-11-13 21:54:46 +0300

    444

--- Ex004/Program.cs
+++ Ex004/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberA = new Random().Next(1, 10); // 1,2,3...9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
