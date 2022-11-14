34a82a2d2cb0dccceb4b12d0190aa9f02f647cce
Ulya <uleiven@mail.ru>
2022-11-14 21:54:36 +0300

    Ex06_7

--- Ex004/Program.cs
+++ Ex004/Program.cs
@@ -1,6 +1,12 @@
-﻿int numberA = new Random().Next(1, 10); // 1,2,3...9
-Console.WriteLine(numberA);
-int numberB = new Random().Next(1, 10);
-Console.WriteLine(numberB);
-int result = numberA + numberB;
-Console.WriteLine(result);
+﻿Console.Write("введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username =="Маша")
+{
+Console.WriteLine("Ура,это же Маша");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
